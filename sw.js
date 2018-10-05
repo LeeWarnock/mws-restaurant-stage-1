2c29aab7c06d8c7734fa18a1c6b7ccb2b0b6f3c8
Lee Warnock <clwarnock@gmail.com>
2018-10-04 19:53:34 -0500

    adding to service worker

--- sw.js
+++ sw.js
@@ -5,13 +5,23 @@ self.addEventListener("install", event => {
       return cache
         .addAll([
           "/",
-          "/index.html",
-          "/restaurant.html",
-          "/css/styles.css",
-          "/css/mainstyles.css",
-          "/js/dbhelper.js",
-          "/js/main.js",
-          "/js/restaurant_info.js"
+          "./index.html",
+          "./restaurant.html",
+          "./css/styles.css",
+          "./js/dbhelper.js",
+          "./js/main.js",
+          "./js/restaurant_info.js",
+          "./data/restaurants.json",
+          "./img/1.jpg",
+          "./img/2.jpg",
+          "./img/3.jpg",
+          "./img/4.jpg",
+          "./img/5.jpg",
+          "./img/6.jpg",
+          "./img/7.jpg",
+          "./img/8.jpg",
+          "./img/9.jpg",
+          "./img/10.jpg"
         ])
         .catch(error => {
           console.log("Caches open failed: " + error);
