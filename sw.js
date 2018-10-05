8859874de613efcbcefe8bb4169e1b62495118a7
Lee Warnock <clwarnock@gmail.com>
2018-10-04 19:55:51 -0500

    adjust service worker

--- sw.js
+++ sw.js
@@ -1,36 +1,35 @@
-// Install site assets via service worker
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("restaurantReviews").then(cache => {
-      return cache
-        .addAll([
-          "/",
-          "./index.html",
-          "./restaurant.html",
-          "./css/styles.css",
-          "./js/dbhelper.js",
-          "./js/main.js",
-          "./js/restaurant_info.js",
-          "./data/restaurants.json",
-          "./img/1.jpg",
-          "./img/2.jpg",
-          "./img/3.jpg",
-          "./img/4.jpg",
-          "./img/5.jpg",
-          "./img/6.jpg",
-          "./img/7.jpg",
-          "./img/8.jpg",
-          "./img/9.jpg",
-          "./img/10.jpg"
-        ])
-        .catch(error => {
-          console.log("Caches open failed: " + error);
-        });
-    })
-  );
+self.addEventListener("install", function(event) {
+  // Perform install steps
 });
 
-// Log requests made from parent page
-self.addEventListener("fetch", function(event) {
-  console.log(event.request.url);
+var CACHE_NAME = "restaurant-reviews-cache-v1";
+var urlsToCache = [
+  "/",
+  "./index.html",
+  "./restaurant.html",
+  "./css/styles.css",
+  "./js/dbhelper.js",
+  "./js/main.js",
+  "./js/restaurant_info.js",
+  "./data/restaurants.json",
+  "./img/1.jpg",
+  "./img/2.jpg",
+  "./img/3.jpg",
+  "./img/4.jpg",
+  "./img/5.jpg",
+  "./img/6.jpg",
+  "./img/7.jpg",
+  "./img/8.jpg",
+  "./img/9.jpg",
+  "./img/10.jpg"
+];
+
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
