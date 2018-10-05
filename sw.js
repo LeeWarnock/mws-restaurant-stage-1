ae16a72c2db7e7864688494f8fd79563614ff509
Lee Warnock <clwarnock@gmail.com>
2018-10-04 20:14:03 -0500

    service worker

--- sw.js
+++ sw.js
@@ -33,3 +33,31 @@ self.addEventListener("install", function(event) {
     })
   );
 });
+
+self.addEventListener("activate", function(event) {
+  var cacheWhitelist = ["restaurant-reviews-cache-v1"];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
